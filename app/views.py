5e020b48496d0b5535f03228d7f04260c03f7268
zsky <kabsky9@gmail.com>
2014-06-29 22:04:14 +0800

    前台几个带插件的网页

---
+++ app/views.py
@@ -0,0 +1,30 @@
+from flask import render_template, redirect, request
+from app import app
+from datetime import datetime
+import pdb
+
+
+
+@app.route('/home')
+def home():
+    return render_template("home.html")
+
+@app.route('/list_type1')
+def list_type1():
+    return render_template("list_type1.html")
+
+@app.route('/regions')
+def regions():
+    return render_template("regions.html")
+
+@app.route('/editor')
+def editor():
+    return render_template("editor.html")
+
+@app.route('/editor_content', methods=["post"])
+def editor_content():
+    print 'content'
+    print request.form['content']
+    return request.form['content']
+
+
